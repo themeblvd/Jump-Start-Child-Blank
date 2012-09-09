1d74bacb4947220212038eb1a46a5975a934e03e
Jason Bobich <jasonbobich@gmail.com>
2012-09-09 12:28:18 -0800

    Changed new style.css format.

--- functions.php
+++ functions.php
@@ -3,14 +3,8 @@
 /* Run Theme Blvd framework (required)
 /*-------------------------------------------------------*/
 
-require_once ( get_template_directory() . '/framework/themeblvd.php' );
+require_once( get_template_directory() . '/framework/themeblvd.php' );
 
 /*-------------------------------------------------------*/
 /* Start Child Theme
 /*-------------------------------------------------------*/
-
-/**
- * Add custom stylesheet after all framework and Jump Start CSS.
- */
-
-themeblvd_add_stylesheet( 'custom', get_stylesheet_directory_uri() . '/assets/css/custom.css', 4 );
